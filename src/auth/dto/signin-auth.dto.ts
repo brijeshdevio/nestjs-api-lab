b164241c5bc4a158741a8cea38553f054d953e63
Brijesh <softbrijeshio@gmail.com>
2025-10-03 16:16:13 +0530

    fix: add signup and signin input validation

--- src/auth/dto/signin-auth.dto.ts
+++ src/auth/dto/signin-auth.dto.ts
@@ -1,4 +1,12 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export default class SignInAuthDto {
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString()
   password: string;
 }
--- src/auth/dto/signup-auth.dto.ts
+++ src/auth/dto/signup-auth.dto.ts
@@ -1,5 +1,22 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
 export default class SignUpAuthDto {
+  @IsNotEmpty({ message: 'Name is required' })
+  @IsString()
   name: string;
+
+  @IsNotEmpty({ message: 'Email is required' })
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty({ message: 'Password is required' })
+  @IsString()
+  @IsStrongPassword()
   password: string;
 }
