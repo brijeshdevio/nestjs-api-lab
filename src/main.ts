c2d6c8c6f423a75aec3a54138283df58d03d5b5c
Brijesh <softbrijeshio@gmail.com>
2025-10-03 15:59:50 +0530

    feat(docs): add OpenAPI swagger Docs

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJS API Lab!')
+    .setDescription(
+      `A collection of **small backend experiments and API modules** built using **NestJS**, designed to explore real-world backend concepts like authentication, CRUD, guards, pipes, and Prisma integration.`,
+    )
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
